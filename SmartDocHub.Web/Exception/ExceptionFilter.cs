55e3425e753ff39f2401b16eed1a255b2d2d7a24
xztimer <xztimer@outlook.com>
2026-07-06 18:00:57 +0800

    identity引入，用户登录注册

--- SmartDocHub.Web/Exception/ExceptionFilter.cs
+++ SmartDocHub.Web/Exception/ExceptionFilter.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc.Filters;
 
 using SmartDocHub.Service.Exceptions;
-using SmartDocHub.Web.Reponse;
 
 namespace SmartDocHub.Web.Exception;
 
@@ -18,15 +17,13 @@ public class ExceptionFilter : IAsyncExceptionFilter
     {
         if (context.Exception is BusinessException businessException)
         {
-            var res = new ApiResult<object>(businessException.Code, businessException.Message);
-            context.Result = new OkObjectResult(res);
+            context.Result = new OkObjectResult(1);
             _logger.LogWarning($"业务异常触发: {businessException.Message}");
         }
         else
         {
             _logger.LogError(context.Exception, "系统发生未捕获的未预期异常");
-            var res = new ApiResult<object>(ResponseCode.ServerError, "服务器发生了未知错误");
-            context.Result = new ObjectResult(res) { StatusCode = 500 };
+            context.Result = new ObjectResult(1) { StatusCode = 500 };
         }
 
         context.ExceptionHandled = true;

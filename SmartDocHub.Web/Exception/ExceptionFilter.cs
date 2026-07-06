35c5ce79394f184a4d277362b6eed2ec71db3efc
xztimer <xztimer@outlook.com>
2026-07-06 18:05:17 +0800

    修复

--- SmartDocHub.Web/Exception/ExceptionFilter.cs
+++ SmartDocHub.Web/Exception/ExceptionFilter.cs
@@ -18,15 +18,15 @@ public class ExceptionFilter : IAsyncExceptionFilter
     {
         if (context.Exception is BusinessException businessException)
         {
-            var res = new ApiResult<object>(businessException.Code, businessException.Message);
-            context.Result = new OkObjectResult(res);
+            //var res = new ApiResult<object>(businessException.Code, businessException.Message);
+            //context.Result = new OkObjectResult(res);
             _logger.LogWarning($"业务异常触发: {businessException.Message}");
         }
         else
         {
             _logger.LogError(context.Exception, "系统发生未捕获的未预期异常");
-            var res = new ApiResult<object>(ResponseCode.ServerError, "服务器发生了未知错误");
-            context.Result = new ObjectResult(res) { StatusCode = 500 };
+            //var res = new ApiResult<object>(ResponseCode.ServerError, "服务器发生了未知错误");
+            //context.Result = new ObjectResult(res) { StatusCode = 500 };
         }
 
         context.ExceptionHandled = true;

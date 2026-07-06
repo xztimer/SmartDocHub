4f6dd3f22d8053e58bf45fec05bb6ebb1705c9f8
xztimer <xztimer@outlook.com>
2026-07-06 18:01:31 +0800

    Revert "identity引入，用户登录注册"
    This reverts commit 55e3425e753ff39f2401b16eed1a255b2d2d7a24.

--- SmartDocHub.Web/Exception/ExceptionFilter.cs
+++ SmartDocHub.Web/Exception/ExceptionFilter.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Filters;
 
 using SmartDocHub.Service.Exceptions;
+using SmartDocHub.Web.Reponse;
 
 namespace SmartDocHub.Web.Exception;
 
@@ -17,13 +18,15 @@ public class ExceptionFilter : IAsyncExceptionFilter
     {
         if (context.Exception is BusinessException businessException)
         {
-            context.Result = new OkObjectResult(1);
+            var res = new ApiResult<object>(businessException.Code, businessException.Message);
+            context.Result = new OkObjectResult(res);
             _logger.LogWarning($"业务异常触发: {businessException.Message}");
         }
         else
         {
             _logger.LogError(context.Exception, "系统发生未捕获的未预期异常");
-            context.Result = new ObjectResult(1) { StatusCode = 500 };
+            var res = new ApiResult<object>(ResponseCode.ServerError, "服务器发生了未知错误");
+            context.Result = new ObjectResult(res) { StatusCode = 500 };
         }
 
         context.ExceptionHandled = true;
